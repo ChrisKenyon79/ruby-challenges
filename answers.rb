9ea3a7bccf17c23c111e269cc7d79610b26b74fe
ChrisKenyon79 <ckenyon79@gmail.com>
2016-08-30 08:04:32 -0700

    almost done

---
+++ answers.rb
@@ -0,0 +1,122 @@
+
+
+#RUBY CHALLENGES DELIVERABLE
+
+#CHRIS KENYON
+#WDI-09
+#August 29-30 2016
+
+
+def my_function()
+
+puts "What calculation would you like to do? (add, sub, div, mult)"
+
+entry = gets
+
+entry = entry.chomp
+
+if entry == 'add'
+  puts "What is number 1"
+  num1 = gets
+  puts "What is number 2"
+  num2 = gets
+  answer = num1 + num2
+  puts answer
+elsif entry == 'sub'
+  puts "What is number 1"
+  num1 = gets
+  puts "What is number 2"
+  num2 = gets
+  answer = num1 - num2
+  puts answer
+elsif entry == 'div'
+  puts "What is number 1"
+  num1 = gets
+  puts "What is number 2"
+  num2 = gets
+  answer = num1 / num2
+  puts answer
+elsif entry == 'mult'
+  puts "What is number 1"
+  num1 = gets
+  puts "What is number 2"
+  num2 = gets
+  answer = num1 * num2
+  puts answer  
+else
+  puts "Not a calculator function"
+
+end
+
+my_function()
+
+
+
+
+
+
+
+
+
+
+
+def my_reverse(my_string)
+
+  #convert string to array
+  #pop or shift, save into new array
+  #reconvert answer array
+
+end
+
+
+
+
+
+
+
+
+def my_function()
+
+puts "What calculation would you like to do? (add, sub, div, mult)"
+
+entry = gets
+
+entry = entry.chomp
+
+if entry == 'add'
+  puts "What is number 1"
+  num1 = gets
+  puts "What is number 2"
+  num2 = gets
+  answer = num1 + num2
+  puts answer
+elsif entry == 'sub'
+  puts "What is number 1"
+  num1 = gets
+  puts "What is number 2"
+  num2 = gets
+  answer = num1 - num2
+  puts answer
+elsif entry == 'div'
+  puts "What is number 1"
+  num1 = gets
+  puts "What is number 2"
+  num2 = gets
+  answer = num1 / num2
+  puts answer
+elsif entry == 'mult'
+  puts "What is number 1"
+  num1 = gets
+  puts "What is number 2"
+  num2 = gets
+  answer = num1 * num2
+  puts answer  
+else
+  puts "Not a calculator function"
+
+end
+
+my_function()
+
+
+
