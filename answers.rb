b823be43e53ac847ba611bd56c045dc65a99f092
ChrisKenyon79 <ckenyon79@gmail.com>
2016-08-30 08:46:23 -0700

    final, runtime errors

--- answers.rb
+++ answers.rb
@@ -62,9 +62,16 @@ my_function()
 
 def my_reverse(my_string)
 
-  #convert string to array
-  #pop or shift, save into new array
-  #reconvert answer array
+  my_string.split("")!
+  answer_array = []
+
+  while my_string != []
+    answer_array.push(my_string.pop!)
+  end
+
+  answer_array.to_s!
+
+  puts answer_array
 
 end
 
@@ -75,48 +82,73 @@ end
 
 
 
-def my_function()
+def my_bank_function()
 
-puts "What calculation would you like to do? (add, sub, div, mult)"
+deposited = 0
+
+puts "Your current balance is "
+puts deposited
+
+puts "What would you like to do? (deposit, withdraw, balance?)"
 
 entry = gets
-
 entry = entry.chomp
 
-if entry == 'add'
-  puts "What is number 1"
+if entry == 'deposit'
+  puts "How much would you like to deposit?"
   num1 = gets
-  puts "What is number 2"
-  num2 = gets
-  answer = num1 + num2
-  puts answer
-elsif entry == 'sub'
-  puts "What is number 1"
+  deposited = deposited + num1
+  puts "You have a total of "
+  puts deposited
+elsif entry == 'withdraw'
+  puts "How much would you like to withdraw"
   num1 = gets
-  puts "What is number 2"
-  num2 = gets
-  answer = num1 - num2
-  puts answer
-elsif entry == 'div'
-  puts "What is number 1"
-  num1 = gets
-  puts "What is number 2"
-  num2 = gets
-  answer = num1 / num2
-  puts answer
-elsif entry == 'mult'
-  puts "What is number 1"
-  num1 = gets
-  puts "What is number 2"
-  num2 = gets
-  answer = num1 * num2
-  puts answer  
+  deposited = deposited - num1
+  puts "You have a total of "
+  puts deposited
+elsif entry == 'balance'
+  puts "Your balance is "
+  puts deposited
 else
-  puts "Not a calculator function"
+  puts "Not a bank function"
 
 end
 
-my_function()
+my_bank_function()
+
+
+
+
+
+
+
+
+
+
+def my_guessing_function
+
+
+answer.rand(1..100)
+winner = false
+try = 0
+iteration = 0
+
+while winner == false
+  puts "What is your guess?"
+  gets try
+  iteration += 1
+
+  if try = answer
+    winner = true
+    puts "You win, you took #{iteration} guesses"
+  elsif try > answer
+    puts "Too high"
+  elsif try < answer
+    puts "Too low"
+  else
+    puts "error debug flag 1"
+  end
+end
 
 
 
